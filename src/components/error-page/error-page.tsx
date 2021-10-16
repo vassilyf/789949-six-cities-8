acb101429cd968cd2cd17a078909670c47565224
vassilyf <vassilyf@mail.ru>
2021-10-16 08:35:39 +0300

    add routing

---
+++ src/components/error-page/error-page.tsx
@@ -0,0 +1,14 @@
+
+function ErrorPage(): JSX.Element {
+  return (
+    <main>
+      <div className="container">
+        <section >
+          <h2>Page not found</h2>
+        </section >
+      </div>
+    </main>
+  );
+}
+
+export default ErrorPage;
