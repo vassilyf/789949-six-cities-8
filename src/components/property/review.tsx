8cb3f87c5d07a4e7e2c7ebbe933b1f4de321587c
vassilyf <vassilyf@mail.ru>
2021-10-24 08:23:47 +0300

    add map to the Room component

---
+++ src/components/property/review.tsx
@@ -0,0 +1,30 @@
+import { Comment } from '../../types/types';
+
+type ReviewProps = {
+  comment: Comment,
+}
+
+export function Review({comment}: ReviewProps): JSX.Element {
+  const reviewDateStr = `${comment.date.getFullYear()}-${comment.date.getMonth()}-${comment.date.getDay()}`;
+  const reviewMonth = `${comment.date.toLocaleString('default', { month: 'long' })} ${comment.date.getFullYear()}`;
+  return (
+    <li className="reviews__item">
+      <div className="reviews__user user">
+        <div className="reviews__avatar-wrapper user__avatar-wrapper">
+          <img className="reviews__avatar user__avatar" src={comment.user.avatar_url} width="54" height="54" alt="Reviews avatar"/>
+        </div>
+        <span className="reviews__user-name">{comment.user.name}</span>
+      </div>
+      <div className="reviews__info">
+        <div className="reviews__rating rating">
+          <div className="reviews__stars rating__stars">
+            <span style={{width: '80%'}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <p className="reviews__text">{comment.comment}</p>
+        <time className="reviews__time" dateTime={reviewDateStr}>{reviewMonth}</time>
+      </div>
+    </li>
+  );
+}
